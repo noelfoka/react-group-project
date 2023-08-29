853788094f6a830f96305b428aa2b7e88502d779
suleiman gacheru <dretrevor8@gmail.com>
2023-08-29 13:38:20 +0300

    Add mission layout

--- src/components/pages/Missions.js
+++ src/components/pages/Missions.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React from 'react'
 
-const Missions = () => <div>Missions</div>;
+export const Missions = () => {
+  return (
+  <>
+  <tr>
+      <td> Mission Name </td>
+      <td> Description </td>
+      <td>
+        <span>Active Member</span>
+        <span>Not A Member</span>
+      </td>
+      <td>
+        <span>Mission Button</span>
+      </td>
+    </tr>
+  </>
+  )
+}
 
 export default Missions;
